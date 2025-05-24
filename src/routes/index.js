48f1c03b8a731f3bbc5a4a31bdfb0be1ce33c736
Rodrigo Alexander Sabino Ramirez <u20201b801@upc.edu.pe>
2025-05-24 03:18:44 -0500

    feat(book): implement creation and search by name or ISBN with validation and error handling

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,9 +1,13 @@
 const express = require('express');
 const router = express.Router();
+const bookController = require('../controllers/bookController');
 
-// Aquí irán tus rutas
+// Ruta básica para prueba
 router.get('/', (req, res) => {
   res.json({ message: 'API Bookstore funcionando 🎉' });
 });
 
+// Ruta para obtener todos los libros
+router.get('/books', bookController.getAllBooks);
+router.post('/books', bookController.createBook);
 module.exports = router;
